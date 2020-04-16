d95883685cc9c919c18e17dc449a9fa9ab63b911
Emil Jimenez <ejimenez@magnidev.com>
2020-04-16 17:43:44 -0400

    Initial Commit

---
+++ src/main/java/me/phsyco3/raidorganizer/RaidOrganizer.java
@@ -0,0 +1,14 @@
+package me.phsyco3.raidorganizer;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+/**
+ * Using the Foundation Framework
+ * https://github.com/kangarko/Foundation
+ */
+public class RaidOrganizer extends SimplePlugin {
+
+    @Override
+    protected void onPluginStart() {
+    }
+}
